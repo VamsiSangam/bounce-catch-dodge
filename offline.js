6a63a3e1ea670ade2eef5db692bbacb894eda5c9
Vamsi Sangam <vamsisangam@live.com>
2017-09-19 22:12:42 +0530

    Added project files

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1505838694,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/platforml-sheet0.png",
+		"images/platformm-sheet0.png",
+		"images/platforms-sheet0.png",
+		"images/player-sheet0.png",
+		"images/turret-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/spikes-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
